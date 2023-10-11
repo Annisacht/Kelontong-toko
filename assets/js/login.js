15bb8a2c02d4d3eb285cbc4e2ff2f04f0c674443
azismaulana1883 <azis.maulana1883@gmail.com>
2023-10-11 16:50:56 +0700

    penambahan validasi untuk data yang sudah ter get di API

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,18 @@
 // dapatkan id btn dan input didalamnya
 // ambil data api
-// ketika data api sudah didaptkan email dan password field di api bandingkan dengan yang di inputkan user dan buat validasi
+// ketika data api sudah didaptkan email dan password field di api bandingkan dengan yang di inputkan user dan buat validasi sebagai unique field gunakan email
+let usersData = [];
+
+async function getData ()
+{
+    try {
+        usersData = await fetch('https://65261c4e67cfb1e59ce7e741.mockapi.io/users').then((response) => response.json())
+        console.log(usersData);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
 let BtnSubmit = document.getElementById('btn_submit').addEventListener('click', function(event){
     event.preventDefault()
@@ -9,16 +21,16 @@ let BtnSubmit = document.getElementById('btn_submit').addEventListener('click', 
     console.log(elementEmail);
     let elementPassword = document.getElementById('password').value;
     console.log(elementPassword);
+
+    let userisTrue = usersData.find(user => user.email === elementEmail && user.password === elementPassword);
+
+    if (userisTrue) {
+        alert('Login berhasil!');
+        window.location.href = '../index.html';
+    } else {
+        alert('Email atau password salah.');
+    }
 });
 
-async function getData ()
-{
-    try {
-        let dataUserApi = await fetch('https://65261c4e67cfb1e59ce7e741.mockapi.io/users').then((response) => response.json())
-        console.log(dataUserApi);
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 getData()
+
