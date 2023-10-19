2e1fbd69c2d4748e1991b133f3cd3da47fc62c89
Annisa Samanhudi <145771445+Annisacht@users.noreply.github.com>
2023-10-19 12:29:15 +0700

    cart 2

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,21 +1,43 @@
+//profile
+async function fetchData() {
+  const response = await fetch('https://65261c4e67cfb1e59ce7e741.mockapi.io/users');
+  const data = await response.json();
+
+  const navbarProfile = document.getElementById('profile');
+  const user = data[0];
+  const profile = document.createElement('p');
+  profile.textContent = `Hello, ${user.nama}!`;
+  navbarProfile.appendChild(profile);
+}
+fetchData();
+
 // form quantity
-const minus = document.querySelector(".minus"),
-plus = document.querySelector(".plus"),
-num = document.querySelector(".num");
+  const plusButton = document.querySelector('.plus');
+  const minusButton = document.querySelector('.minus');
+  const quantityElement = document.querySelector('.num');
+  const priceElement = document.querySelector('.total-price');
 
-let a = 1;
+  // Harga per produk
+  const hargaProduk = 58000;
 
-plus.addEventListener("click",()=> {
-    a++;
-    a = (a < 10) ? "" + a : a;
-    num.innerText = a;
-    console.log(a);
-});
+  plusButton.addEventListener('click', function () {
+    let jumlah = parseInt(quantityElement.textContent);
+    jumlah++;
+    quantityElement.textContent = jumlah;
 
-minus.addEventListener("click",()=> {
-  if (a > 1){
-    a--;
-    a = (a < 10) ? "" + a : a;
-    num.innerText = a;
-  }
-});
+    let hargaTotal = jumlah * hargaProduk;
+    priceElement.textContent = 'Total: Rp ' + hargaTotal.toLocaleString();
+  });
+
+
+  minusButton.addEventListener('click', function () {
+    let jumlah = parseInt(quantityElement.textContent);
+    if (jumlah > 1) {
+      jumlah--;
+      quantityElement.textContent = jumlah;
+
+      let hargaTotal = jumlah * hargaProduk;
+      priceElement.textContent = 'Total: Rp ' + hargaTotal.toLocaleString();
+    }
+  });
+
