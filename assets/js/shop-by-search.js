16d18a63d76036d3fe14382ea8927b4783ff5e72
Annisa Samanhudi <145771445+Annisacht@users.noreply.github.com>
2023-10-19 12:47:15 +0700

    ubah nav

--- assets/js/shop-by-search.js
+++ assets/js/shop-by-search.js
@@ -1,23 +1,11 @@
-// const getDataApi = "https://65261c4e67cfb1e59ce7e741.mockapi.io/users"
-
-// fetch (getDataApi, {
-//     method: "POST",
-
-// })
-// .then((result) => result.json())
-// .then((data) => console.log(data))
-
-
 async function fetchData() {
     const response = await fetch('https://65261c4e67cfb1e59ce7e741.mockapi.io/users');
     const data = await response.json();
 
-    const profile = document.getElementById('profile');
-    data.forEach(data => {
-        const profile = document.createElement('p');
-        profile.textContent = `${data.nama}`;
-        dataList.appendChild(profile);
-    });
+    const navbarProfile = document.getElementById('profile');
+    const user = data[0];
+    const profile = document.createElement('p');
+    profile.textContent = `Hello, ${user.nama}!`;
+    navbarProfile.appendChild(profile);
 }
-
 fetchData();
