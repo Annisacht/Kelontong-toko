b4f9222a036eac75c43eb8e020fbb3540f225823
Annisa Samanhudi <145771445+Annisacht@users.noreply.github.com>
2023-10-20 21:25:31 +0700

    #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ function filterProduct() {
             productsRow.innerHTML += `
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="assets/img/${product.image}" class="card-img-top" alt="${product.name}">
+                        <img src="${product.image}" class="card-img-top" alt="${product.name}">
                         <div class="card-body">
                           <h5 class="card-title">${product.name}</h5>
                           <p class="card-text">${product.category}</p>
